4ef65f44c41cac2404ed24dc6c294ba66d81132e
BryanTacuri <bryantacuriar@gmail.com>
2022-07-01 01:01:00 -0500

    Se añadio SharedPreference login

--- PizzaMellisos/app/src/main/java/com/example/pizzamellisos/IniciarSesionActivity.java
+++ PizzaMellisos/app/src/main/java/com/example/pizzamellisos/IniciarSesionActivity.java
@@ -3,9 +3,14 @@ package com.example.pizzamellisos;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,6 +24,12 @@ public class IniciarSesionActivity extends AppCompatActivity {
     private EditText correo;
     private EditText password;
     private FirebaseAuth mAuth;
+    private Button btnLogin;
+
+    private CheckBox checkGuardarSesion;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    private String llave = "sesion";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,27 +38,35 @@ public class IniciarSesionActivity extends AppCompatActivity {
         correo = findViewById(R.id.btnCorreoLogin);
         password = findViewById(R.id.btnPasswordLogin);
         mAuth = FirebaseAuth.getInstance();
+
+        btnLogin = findViewById(R.id.btnLogin);
+        checkGuardarSesion = findViewById(R.id.checkSesionActiva);
+
+        preferences = getSharedPreferences("sesiones", Context.MODE_PRIVATE);
+        editor = preferences.edit();
+
     }
         public void login(View view) {
+            MainActivity main = new MainActivity();
 
-            Intent i = new Intent(getApplicationContext(), PantallaPrincipalActiviry.class);
-            i.putExtra("sms", "Datos correctos");
-            startActivity(i);
-            finish();
-
-            /*
             if ((correo.getText().length() == 0) || (password.getText().length() == 0)) {
                 Toast.makeText(this, "Ingrese todos los campos", Toast.LENGTH_SHORT).show();
 
             } else {
+
                 mAuth.signInWithEmailAndPassword(correo.getText().toString(), password.getText().toString())
                         .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
+
                                     FirebaseUser user = mAuth.getCurrentUser();
+                                    MainActivity main = new MainActivity();
+                                    if(checkGuardarSesion.isChecked()){
+                                       guardarSesion(checkGuardarSesion.isChecked(), correo.getText().toString(), password.getText().toString());
+                                    }
+
                                     Intent i = new Intent(getApplicationContext(), PantallaPrincipalActiviry.class);
-                                    i.putExtra("sms", "Datos correctos");
                                     startActivity(i);
                                     finish();
                                     //updateUI(user);
@@ -62,7 +81,7 @@ public class IniciarSesionActivity extends AppCompatActivity {
                                 }
                             }
                         });
-            }*/
+            }
         }
 
         public void onStart() {
@@ -72,9 +91,20 @@ public class IniciarSesionActivity extends AppCompatActivity {
             //updateUI(currentUser);
         }
 
+
+    public void guardarSesion(boolean checked, String nombre, String clave){
+        editor.putBoolean(llave,  checked);
+        editor.putString("nombre", nombre);
+        editor.putString("clave", clave);
+        editor.apply();
+
+    }
+
         public void cancelar(View view){
             Intent i = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(i);
         }
+
+
 
     }
--- PizzaMellisos/app/src/main/java/com/example/pizzamellisos/MainActivity.java
+++ PizzaMellisos/app/src/main/java/com/example/pizzamellisos/MainActivity.java
@@ -2,16 +2,33 @@ package com.example.pizzamellisos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private String llave = "sesion";
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        preferences = getSharedPreferences("sesiones", Context.MODE_PRIVATE);
+        editor = preferences.edit();
+
+        if(revisarSesion()){
+            Intent i = new Intent(this, PantallaPrincipalActiviry.class);
+            startActivity(i);
+        }
+
     }
 
 
@@ -24,4 +41,11 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, RegistrarseActivity.class);
         startActivity(i);
     }
+
+
+    public boolean revisarSesion(){
+        return this.preferences.getBoolean(llave, false);
+    }
+
+
 }
