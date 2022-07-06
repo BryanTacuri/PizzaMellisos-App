63b0876e968d4e42835b9268f6a0fe89464c5579
Daniel-Pincay <86489085+El-Buen-Alex@users.noreply.github.com>
2022-07-05 20:04:07 -0500

    done

--- PizzaMellisos/app/src/main/java/com/example/pizzamellisos/RegistrarPromocionActivity.java
+++ PizzaMellisos/app/src/main/java/com/example/pizzamellisos/RegistrarPromocionActivity.java
@@ -1,5 +1,8 @@
 package com.example.pizzamellisos;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -12,6 +15,8 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -24,13 +29,18 @@ public class RegistrarPromocionActivity extends AppCompatActivity {
     private RadioButton rbmasculino, rbfemenino;
     private RadioGroup radioGroup;
     private String seleccion;
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
+    private static final String TAG = "MainActivity";
+    private static final String[] PERMISSIONS = {android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.INTERNET};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_registrar_promocion);
-
+        ActivityCompat.requestPermissions(this, PERMISSIONS, 112);
         nombre = findViewById(R.id.txtNombreRegister);
         apellido = findViewById(R.id.txtApellidoRegister);
         edad = findViewById(R.id.txtEdadRegister);
@@ -58,10 +68,17 @@ public class RegistrarPromocionActivity extends AppCompatActivity {
         if(statusSD == 0) {
             try {
                 File ruta_sd = Environment.getExternalStorageDirectory();
+                System.out.println("ey");
+
                 File raizSD = new File(ruta_sd.getAbsolutePath(), "datos_sd.txt");
-                OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(raizSD));
-                osw.write(data);
-                osw.close();
+                if(!raizSD.exists()){
+                    raizSD.createNewFile();
+                }
+
+                if(hasPermissions(this, PERMISSIONS)){
+                    OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(raizSD));
+                    osw.write(data);
+                    osw.close();
                 /*fileOutputStream = openFileOutput("datos_sd.txt", MODE_PRIVATE);
                 fileOutputStream.write(data.getBytes());
                 File ruta_sd = new File(Environment.getExternalStorageState());
@@ -69,7 +86,7 @@ public class RegistrarPromocionActivity extends AppCompatActivity {
                 OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(ruta_sd));
                 osw.write(data);
                 osw.close();*/
-                Toast.makeText(getApplicationContext(), "Guardado", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Guardado", Toast.LENGTH_SHORT).show();
                 /*File raizSD = new File(ruta_sd.getAbsolutePath(), "datos_sd.txt");
                 FileOutputStream f = new FileOutputStream(raizSD);
 
@@ -81,8 +98,11 @@ public class RegistrarPromocionActivity extends AppCompatActivity {
                 f.close();
                 Toast.makeText(getApplicationContext(), "Guardado", Toast.LENGTH_SHORT).show();
                 Log.d("TAG1", "Guardado en: "+f);*/
+                }
+
+
             } catch (Exception e) {
-                Log.e("Ficheros", "Error al escribir ficheros a tarjeta SD");
+                Log.e("Ficheros", "Error al escribir ficheros a tarjeta SD" +e.toString());
             } /*finally {
                 if (fileOutputStream != null){
                     try{
@@ -97,6 +117,34 @@ public class RegistrarPromocionActivity extends AppCompatActivity {
         }
     }
 
+
+
+    private static boolean hasPermissions(Context context, String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private void requestPermission() {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(this, "Write External Storage permission allows us to do store images. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
     private int verificarStatusSD(){
         String estado = Environment.getExternalStorageState();
         if(estado.equals(Environment.MEDIA_MOUNTED)){
